334bfe173a8cb6ebd4d6eefaa6bca38b4ce9ae62
MatheusSFDev <mateussousa909091@gmail.com>
2025-05-16 11:40:43 +0100

    Desestructuración

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,9 @@
 //Importación del modulo
 const saludo = require("./saludos.js");
-
 console.log(saludo.saludar("Matheus probando Node.js"));
 console.log(saludo.saludarMundo());
+
+//Desestructuración del modulo
+const {saludar, saludarMundo} = require("./saludos.js");
+console.log(saludar("Matheus probando Node.js"));
+console.log(saludarMundo());

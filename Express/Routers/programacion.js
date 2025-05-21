a6960aea71f5d3eb43bbb9d5f6f318a9747a6d81
MatheusSFDev <mateussousa909091@gmail.com>
2025-05-21 17:27:55 +0100

    Metodos HTTP

--- Express/Routers/programacion.js
+++ Express/Routers/programacion.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {programacion} = require("../datos/cursos.js").infoCursos;
 const router = express.Router();
+router.use(express.json());
 
 router.get("", (req, res) => {
-    res.send(JSON.stringify(programacion));
+    res.json(programacion);
     console.log("Solicitud Respondida: /api/cursos/programacion ");
 });
 
@@ -12,7 +13,7 @@ router.get("/:lenguaje", (req, res) => {
     const resultados = programacion.filter(curso => curso.lenguaje === lenguaje);
 
     if (resultados.length === 0) {
-        return res.status(404).send("No se Encuentra");
+        return res.status(404).end();
     }
 
     if (req.query.ordenar === "vistas") {
@@ -36,6 +37,52 @@ router.get("/:lenguaje/:nivel", (req, res) => {
 
     res.send(JSON.stringify(resultados));
     console.log("Solicitud Respondida: /api/cursos/programacion/:lenguaje/:nivel ");
+});
+
+router.post("", (req, res) => {
+    console.log("Solicitud POST Recebida");
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));
+    console.log("Solicitud POST Realizada");
+});
+
+router.put("/:id", (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        programacion[indice] = cursoActualizado;
+    }
+
+    res.send(JSON.stringify(programacion));
+});
+
+router.patch("/:id", (req, res) => {
+    const info = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        const cursoA = programacion[indice];
+        Object.assign(cursoA, info);
+    }
+
+    res.send(JSON.stringify(programacion));
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        programacion.splice(indice, 1);
+    }
+
+    res.send(JSON.stringify(programacion));
 });
 
 module.exports = router;
